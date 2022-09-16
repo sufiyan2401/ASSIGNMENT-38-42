82cd72bd4ab9e2e3544e6682a81e5bfee3ac2ce8
Sufiyan <110626687+sufiyan2401@users.noreply.github.com>
2022-09-16 23:11:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+// CHAPTER 38 - 42 //
+//ASSIGNMENT 1//
+//NT UNDERSTANDING
+
+//ASSIGNMENT 2//
+// var a = prompt("Enter Year 2012 - 2020")
+// if(a == "2012"){
+//     alert("This is Leap Year")
+// }
+// else if (a == "2016"){
+//     alert("This is Leap Year")
+// }
+// else if (a == "2020"){
+//     alert("This is Leap Year")
+// }
+// else if (a){
+//     alert("This is Not A Leap Year")
+// }
+
+// ASSIGNMENT 3 // 
+// nt understand
+
+// // ASSIGNMENT 4 //
+// var m = prompt("Enter your Math Marks")
+// var c = prompt("Enter your Chemistry Marks")
+// var p = prompt("Enter your Physics Marks")
+// var sub = Number(m)+Number(c)+Number(p);
+// var percentage=(sub*100/300)
+// document.write("Your Percentage Is"       +  percentage)
+
+// // ASSIGNMENT 4//
+//  var firstName = prompt("Enter First Name");
+//  var lastName = prompt("Enter Last Name");
+//  var email = prompt("Enter Email Name");
+
+//  function bioData(firstName, lastName, email){
+//      document.write("My First Name: " + firstName + "<br>")
+//      document.write("My Last Name: " + lastName + "<br>")
+//      document.write("My Email Address: " + email)
+//  }
+
+//  bioData(firstName, lastName, email)
+
+// // ASSIGNMENT 5 //
+// function abc(){
+//     var a = "Hello! This is my Function"
+//     return a;
+// }
+// document.write(abc())
+
+// ASSIGNMENT 6 //
+// not studied//
+
+//ASSIGNMENT 7//
+//nt done // 
+
+// ASSIGNMENT 8//
+//FUNCTION FOR METER
+// function a1(){
+//     var km = prompt("Enter Distance in km")
+//      var m = Number(km)*1000
+//      return m;
+// }
+// var a = (a1()) +  ("Meters")
+// alert(a)
+//FUCTION FOR FEET
+// function a2(){
+//     var km1 = prompt("Enter Distance in km")
+//      var m = Number(km1)*3281
+//      return m;
+// }
+// var b = (a2()) +  ("Feet")
+// alert(b)
+// FUNCTION FOR INCHES
+// function a3(){
+//     var km3 = prompt("Enter Distance in km")
+//      var m = Number(km3)*39370
+//      return m;
+// }
+// var c = (a3()) +  ("Inches")
+// alert(c)
+// FUNCTION FOR CENTIMETER
+// function a4(){
+//     var km4 = prompt("Enter Distance in km")
+//      var m = Number(km4)*100000
+//      return m;
+// }
+// var d = (a4()) +  ("Centimeters")
+// alert(d)
+
+// ASSIGNMET 9 //
